02d375324381d292acac8830965eee3e594ae287
Muhammad Hussein Fattahizadeh <m@mhf.ir>
2019-10-08 21:40:15 +0330

    fix: fix loading json

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const languages = require('./info/languages.json');
 
 const Organization = {};
 languages.forEach((lang) => {
-  Organization[lang] = fs.readFileSync(`./info/${lang}/Organization.json`);
+  Organization[lang] = JSON.parse(fs.readFileSync(`${__dirname}/info/${lang}/Organization.json`, {
+    encoding: 'utf8',
+  }));
 });
 
 module.exports = {
